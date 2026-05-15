e05e0de7772a2359d5bfcd5a5b9d2d5485298265
Codedby-AC <2024btechaidsaishwary18921@poornima.edu.in>
2026-05-15 22:56:03 +0530

    Completed CRUD APIs with MYSQL integration

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,22 +8,76 @@ from app.schemas.user import UserCreate
 router = APIRouter()
 
 @router.post("/users")
-
 def create_user(user: UserCreate):
-    db: Session  = SessionLocal()
-    
+
+    db: Session = SessionLocal()
+
     new_user = User(
-        name = user.name,
-        email = user.email,
-        password = user.password
-        )
-    
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )
+
     db.add(new_user)
-    
+
     db.commit()
-    
+
     db.refresh(new_user)
-    
-    return{
+
+    return {
         "message": "User created successfully"
+    }
+
+
+@router.get("/users")
+def get_users():
+
+    db: Session = SessionLocal()
+
+    users = db.query(User).all()
+
+    return users
+
+
+@router.put("/users/{user_id}")
+def update_user(user_id: int, updated_user: UserCreate):
+
+    db: Session = SessionLocal()
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        return {
+            "message": "User not found"
         }
+
+    user.name = updated_user.name
+    user.email = updated_user.email
+    user.password = updated_user.password
+
+    db.commit()
+
+    return {
+        "message": "User updated successfully"
+    }
+
+
+@router.delete("/users/{user_id}")
+def delete_user(user_id: int):
+
+    db: Session = SessionLocal()
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        return {
+            "message": "User not found"
+        }
+
+    db.delete(user)
+
+    db.commit()
+
+    return {
+        "message": "User deleted successfully"
+    }
